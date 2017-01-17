4aad8bb047a2eaafef3ed1a78dbba0d7e92bab17
Ed Singleton <singletoned@gmail.com>
2017-01-17 21:02:23 +0000

    Update install script

--- install.py
+++ install.py
@@ -1,16 +1,24 @@
 import os
 
 __here__ = __here__ = os.path.dirname(__file__)
+HOME = os.environ['HOME']
+
+def symlink(p):
+    source = os.path.join(__here__, p)
+    target = os.path.join(HOME, "."+p.split('.symlink')[0])
+    if not os.path.exists(target):
+        print(source, target)
+        # os.symlink(source, target)
 
 def link_files():
     for root, dirs, files in os.walk(__here__):
         for d in list(dirs):
             if d.endswith(".symlink"):
-                symlink_dir(d)
+                symlink(d)
                 dirs.remove(d)
         for f in files:
             if f.endswith(".symlink"):
-                symlink_file(f)
-        pass
+                symlink(f)
 
-
+if __name__ == '__main__':
+    link_files()

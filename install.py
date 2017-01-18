7d5cea30c48ce3bd03e4fb36d7a35a0d04c9573e
Ed Singleton <singletoned@gmail.com>
2017-01-18 10:39:01 +0000

    Update install

--- install.py
+++ install.py
@@ -1,27 +1,28 @@
+from __future__ import print_function
+
 import os
 
-__here__ = __here__ = os.path.dirname(__file__)
+__here__ = __here__ = os.path.abspath(os.path.dirname(__file__))
 HOME = os.environ['HOME']
 
-def symlink(p):
-    source = os.path.join(__here__, p)
+def symlink(root, p):
+    source = os.path.join(root, p)
     target = os.path.join(HOME, "."+p.split('.symlink')[0])
     if not os.path.exists(target):
-        print(source, target)
+        print("Linking %s to %s" % (source, target))
         os.symlink(source, target)
 
 def link_files():
     for root, dirs, files in os.walk(__here__):
         if ".git" in dirs:
             dirs.remove(".git")
-        print(root, dirs, files)
         for d in list(dirs):
             if d.endswith(".symlink"):
-                symlink(d)
+                symlink(root, d)
                 dirs.remove(d)
         for f in files:
             if f.endswith(".symlink"):
-                symlink(f)
+                symlink(root, f)
 
 if __name__ == '__main__':
     link_files()

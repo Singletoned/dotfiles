140e537282f4744eb62e71c135687a155cf7a40a
Ed Singleton <singletoned@gmail.com>
2017-11-14 11:57:37 +0000

    Add create-project alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias mirror="wget -rkmpxE --adjust-extension --random-wait"
 alias docker-clean='docker rm -v $(docker ps -a -q -f status=exited) && docker rmi $(docker images -f "dangling=true" -q)'
+alias create-project='mkvirtualenv ${PWD##*/} && setvirtualenvproject && pip install -r requirements.txt'

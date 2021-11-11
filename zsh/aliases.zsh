4c3464f4988f21162baa0b61c7e936e863f7afe0
Ed Singleton <singletoned@Xander-2.local>
2021-11-11 17:38:16 +0000

    Delete All The Things

--- zsh/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias reload!='. ~/.zshrc'
-alias mirror="wget -rkmpxE --adjust-extension --random-wait"
-alias create-project='mkvirtualenv ${PWD##*/} && setvirtualenvproject && pip install -r requirements.txt'

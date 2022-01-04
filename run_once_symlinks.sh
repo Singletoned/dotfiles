8855138b9c9c968817b47f8e80c68146fd8df343
Ed Singleton <singletoned@gmail.com>
2022-01-04 12:51:05 +0000

    Setup symlinks

---
+++ run_once_symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -d ~/cloud ]; then
+    ln -s "/Users/singletoned/Library/Mobile Documents/com~apple~CloudDocs" ~/cloud
+fi
+if [ ! -d ~/src ]; then
+    ln -s "/Users/singletoned/Library/Mobile Documents/com~apple~CloudDocs/src" ~/src
+fi

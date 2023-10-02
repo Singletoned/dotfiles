5ea40d86371990a8349e43721d7022a0a987de91
Ed Singleton <singletoned@gmail.com>
2023-10-02 19:38:28 +0100

    Update script

--- scripts/install.py
+++ scripts/install.py
@@ -1,34 +1,38 @@
-from __future__ import print_function
-
+import pathlib
 import os
 
-__here__ = __here__ = os.path.abspath(os.path.dirname(__file__))
-HOME = os.environ['HOME']
+__here__ = pathlib.Path(__file__).parent
+BASE_DIR = __here__.parent
+HOME = pathlib.Path(os.environ['HOME']).resolve()
 
 
 def symlink(root, p):
-    base = os.path.join(HOME, "".join(root.split(__here__)).strip("/"))
-    source = os.path.join(root, p)
-    target = os.path.join(base, "."+p.split('.symlink')[0])
-    if not os.path.exists(base):
+    source = root / p
+    base = (HOME / root.relative_to(BASE_DIR)).resolve()
+    target = base /p.stem
+
+    if not base.exists():
         os.makedirs(base)
-    if not os.path.exists(target):
+    if not target.exists():
         print("Linking %s to %s" % (source, target))
         os.symlink(source, target)
 
 
 def link_files():
-    for root, dirs, files in os.walk(__here__):
+    for root, dirs, files in os.walk(BASE_DIR):
+        root = pathlib.Path(root)
         if ".git" in dirs:
             dirs.remove(".git")
-        for d in list(dirs):
-            if d.endswith(".symlink"):
+        for d in tuple(d for d in dirs if not d == ".git"):
+            d = pathlib.Path(d)
+            if d.suffix == ".symlink":
                 symlink(root, d)
-                dirs.remove(d)
         for f in files:
-            if f.endswith(".symlink"):
+            f = pathlib.Path(f)
+            if f.suffix == ".symlink":
                 symlink(root, f)
 
 
 if __name__ == '__main__':
     link_files()
+

c759fe1f9ac6f3b7727cfb90dc0e736ab2bc6119
Ed Singleton <singletoned@Lorne.local>
2023-10-02 17:03:36 +0100

    Update script

--- scripts/setup_virtualenvs.sh
+++ scripts/setup_virtualenvs.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-#
 
 set -o errexit
 set -o nounset
@@ -9,21 +8,19 @@ if [ ! -d ~/.envs ]; then
 
     export PIP_REQUIRE_VIRTUALENV=false
     export VIRTUALENVWRAPPER_PYTHON=python3
+    export WORKON_HOME=$HOME/.envs
 
     python3 -m pip install --upgrade pip virtualenv setuptools
     python3 -m pip install --upgrade virtualenvwrapper
 
-    source virtualenvwrapper.sh
 fi
 
 if [ ! -d ~/.envs/default ]; then
-    mkvirtualenv default
-    workon default
-    python3 -m pip install --upgrade ipython
+    python3 -m venv ~/.envs/default
+    ~/.envs/default/bin/python3 -m pip install --upgrade ipython virtualenvwrapper
 fi
 
 if [ ! -d ~/.envs/emacs ]; then
-    mkvirtualenv emacs
-    workon emacs
-    python3 -m pip install --upgrade black flake8 isort
+    python3 -m venv ~/.envs/emacs
+    ~/.envs/emacs/bin/python3 -m pip install --upgrade black flake8 isort
 fi

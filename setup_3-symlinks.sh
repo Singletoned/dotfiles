875a1990830b2b59a9a016bc62b68c43a9192b84
Ed Singleton <singletoned@Lorne.local>
2023-10-02 18:16:25 +0100

    Delete more files

--- setup_3-symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-if [ ! -d ~/cloud ]; then
-    ln -s "/Users/singletoned/Library/Mobile Documents/com~apple~CloudDocs" ~/cloud
-fi
-if [ ! -d ~/src ]; then
-    ln -s "/Users/singletoned/Library/Mobile Documents/com~apple~CloudDocs/src" ~/src
-fi
--- setup_5-setup-emacs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-set -e
-
-cd ~
-
-if [ ! -d ~/.emacs.d ]; then
-    git clone git@github.com:Singletoned/emacs.d.git ~/.emacs.d
-fi
